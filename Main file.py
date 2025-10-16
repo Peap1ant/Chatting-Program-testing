32a4fc80709154c5cf201961d01fd694389425a3
= <peaplant4@gmail.com>
2025-10-16 11:38:30 +0900

    Create repo

---
+++ Main file.py
@@ -0,0 +1,16 @@
+# ----- import module -----
+
+import tkinter
+import tkinter.font
+
+# ------ main program class ------
+
+class main():
+    def __init__(self):
+        self.root = tkinter.tk()
+
+# ----- ChatApp Layer ------
+
+# ------ Ethernet Layer ------
+
+# ------ Physical Layer ------
